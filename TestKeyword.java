abaabb7fcee4a2943d6141d8604a5f80811b9c4f
Artin Malekian <malekian.artin@gmail.com>
2018-03-03 17:03:36 -0500

    keywordAdd to xml

--- TestKeyword.java
+++ TestKeyword.java
@@ -1,3 +1,4 @@
+
 /*
  * 	Written By: Arin Melakian, Vincent Liu, Amilcar Martinez
  */
@@ -10,65 +11,68 @@ import org.junit.Test;
 import org.junit.Before;
 
 public class TestKeyword {
-    private QuoteList quoteList;
-    private ArrayList<String> keywords;
-    @Before
-    public void set() {
-        //QuoteSaxParser parser = new QuoteSaxParser("quotes.xml");
-    	quoteList = new QuoteList();//parser.getQuoteList();
-    	keywords = new ArrayList<String>();
-    }
-    
-    //Test 1
-    @Test
-    public void testKeywordAdd() {
-        String quote = "Today is Monday";
-        String author = "Amilcar Martinez";
-        //ArrayList<String> keywords = new ArrayList<String>();
-        keywords.add("week");
-        keywords.add("day");
-        quoteserverCLI.appendQuote(quote,author,keywords);
-    }
-    
-    //Test 2
-    @Test
-    public void testQuoteKeyword() {
-       // ArrayList<String> keywords = new ArrayList<String>();
-        keywords.add("week");
-        keywords.add("day");
-        Quote quote = new Quote("author", "quote", keywords);
-    }
-    
-    //Test 3
-    @Test (expected = InvalidParameterException.class)
-    /*
-     *  Tests to see if the quote class will throw an InvalidParameterException 
-     *  when a quote is created with more then 10 keywords
-     */
-    public void testQuoteKeywordLimit(){
-    	for(int x = 0; x < 11;x++)
-    		keywords.add("Keyword"+x);
-    	Quote q = new Quote("Test Author","Test Quote",keywords);
-    }
-    
-	//Test 4
-    @Test	
+	private QuoteList quoteList;
+	private ArrayList<String> keywords;
+
+	@Before
+	public void set() {
+		// QuoteSaxParser parser = new QuoteSaxParser("quotes.xml");
+		quoteList = new QuoteList();// parser.getQuoteList();
+		keywords = new ArrayList<String>();
+	}
+
+	// Test 1
+	@Test
+	public void testKeywordAdd() {
+		String quote = "Today is Monday";
+		String author = "Amilcar Martinez";
+		// ArrayList<String> keywords = new ArrayList<String>();
+		keywords.add("week");
+		keywords.add("day");
+		quoteserverCLI.appendQuote(quote, author, keywords);
+	}
+
+	// Test 2
+	@Test
+	public void testQuoteKeyword() {
+		// ArrayList<String> keywords = new ArrayList<String>();
+		keywords.add("week");
+		keywords.add("day");
+		Quote quote = new Quote("author", "quote", keywords);
+	}
+
+	// Test 3
+	@Test(expected = InvalidParameterException.class)
+	/*
+	 * Tests to see if the quote class will throw an InvalidParameterException when
+	 * a quote is created with more then 10 keywords
+	 */
+	public void testQuoteKeywordLimit() {
+		for (int x = 0; x < 11; x++)
+			keywords.add("Keyword" + x);
+		Quote q = new Quote("Test Author", "Test Quote", keywords);
+	}
+
+	// Test 4
+	@Test
 	public void testKeywordSearchNotEmpty() {
 		QuoteList list = new QuoteList();
-		
-		//add quotes to quotelist
+
+		// add quotes to quotelist
 		String quote = "Today is Monday";
 		String author = "Amilcar Martinez";
 		ArrayList<String> keywords1 = new ArrayList<String>();
 		keywords1.add("week");
 		keywords1.add("day");
-		Quote quote1 = new Quote(author,quote, keywords1);
-		
-		//add quote to quoteList
-		list.setQuote( quote1 );
-		
-		//search for quotes with keyword
+		Quote quote1 = new Quote(author, quote, keywords1);
+
+		// add quote to quoteList
+		list.setQuote(quote1);
+
+		// search for quotes with keyword
 		assertNotEquals(list.search("week", QuoteList.SearchKeyword).getSize(), 0);
 	}
+	
+	
+	
 }
-
