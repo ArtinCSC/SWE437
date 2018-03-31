2d448811acbcf7dedd2109afe37a948a54cf3d6b
amarti74 <35783687+amarti74@users.noreply.github.com>
2018-03-30 21:06:19 -0400

    searchTextNoResultSearchScopeQuote

--- BBCTests.java
+++ BBCTests.java
@@ -1,4 +1,6 @@
 
+import static org.junit.Assert.*;
+
 import java.util.List;
 
 import org.junit.*;
@@ -12,7 +14,7 @@ public class BBCTests {
 	@Before
 	public void setUp() {
 		if(driver == null){
-			System.setProperty("webdriver.gecko.driver","C:\\Users\\Wave\\Downloads\\geckodriver-v0.20.0-win64\\geckodriver.exe");
+			//System.setProperty("webdriver.gecko.driver","C:\Users\Amilcar\Documents\SWE 437\geckodriver-v0.20.0-win64\geckodriver.exe");
 			driver = new FirefoxDriver();
 		}
 		driver.get("https://cs.gmu.edu:8443/offutt/servlet/quotes.quoteserve");
@@ -21,32 +23,42 @@ public class BBCTests {
 	public void cleanUp(){
 		//driver.close();
 	}
-	@Test
-	public void test1(){
-		WebElement searchText = driver.findElement(By.name("searchText"));
-		List<WebElement> searchScopes = driver.findElements(By.name("searchScope"));
-		//WebElement submitButton = driver.findElement(By.name("submit"));
-		for(int x = 0; x < searchScopes.size();x++){
-			String scopeValue = searchScopes.get(x).getAttribute("value");
-			if(scopeValue.equalsIgnoreCase("quote")){
-				searchScopes.get(x).click();
-				break;
-			}
-		}
-		searchText.sendKeys("cow");
-		searchText.submit();
-		//submitButton.click();
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		WebElement results = wait.until( ExpectedConditions.presenceOfElementLocated(By.tagName("dl")) );
-
-		//WebElement results = driver.findElement(By.tagName("dl"));
-		System.out.println(results.getText());
-	}
 	
 	@Test
 	public void searchTextNoResultSeachScopeQuote()
 	{
+		//Expected test results:
+		//		No description list html element. A paragraph element with “did not match any quotes” in its text.
+		//
+		//		The last link in the ‘User Searches’ lists is the test search, with matching search text and scope in the URL.
+
 		String searchString = "1------------------4";
+        //Place focus on search text field
+		WebElement searchTextField = driver.findElement(By.name("searchText"));
+		//Populate search text field
+		searchTextField.sendKeys(searchString);
+		//Place focus on quote radio button
+        WebElement searchScope = driver.findElement(By.id("quote"));
+	    //Select quote radio button
+        searchScope.click();
+        //Submit form
+        searchTextField.submit();
+        //Wait for page to load
+        WebDriverWait wait = new WebDriverWait(driver, 1);
+        WebElement htmlTable = wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("table")));
+        //Checking test result
+        String secondPElement = htmlTable.findElement( By.tagName("tbody")).findElement( By.tagName("tr"))
+        		.findElement(By.tagName("td")).findElement(By.tagName("p")).getText();
+        assertTrue( secondPElement.equals("Your search - " + searchString + " - did not match any quotes."));
+        
+        
+	}
+	
+	public String getLastUserSearchResult(){
+		String lastUserSearchResult = null;
+		WebElement outerTable = driver.findElement( By.tagName("table"));
+		WebElement outerTableData = outerTable.findElement( By.tagName( "tbody")).findElement( By.tagName("tr"));
 		
+		return lastUserSearchResult;
 	}
 }
