cb7fa074561deee21f8b76f373c4fabff56cffdf
Vincent Liu <liuvincent19915@gmail.com>
2018-03-30 22:26:19 -0400

    Added commentes (incomplete)

--- BBCTests.java
+++ BBCTests.java
@@ -29,115 +29,123 @@ public class BBCTests {
 			e.printStackTrace();
 		}
 		driver.close();	}
-/*
-	@Test
-	public void test1(){
-		WebElement searchText = driver.findElement(By.name("searchText"));
-		List<WebElement> searchScopes = driver.findElements(By.name("searchScope"));
-		for(int x = 0; x < searchScopes.size();x++){
-			String scopeValue = searchScopes.get(x).getAttribute("value");
-			if(scopeValue.equalsIgnoreCase("quote")){
-				searchScopes.get(x).click();
-				break;
-			}
-		}
-		searchText.sendKeys("cow");
-		searchText.submit();
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		WebElement results = wait.until( ExpectedConditions.presenceOfElementLocated(By.tagName("dl")) );
 
-		System.out.println(results.getText());
-	}
-*/
+	/*=============================================
+	 * Waits until an element with tag waitTagName
+	 * appears or TimedOut. After finding waitTagName,
+	 * return child elements of waitTagName with tag
+	 * tagName
+	 * @Parameters: String waitTagName, tag to wait for
+	 * 				String tagName, tag to find
+	 * @Return: List<WebElement>, list of web elements found 
+	 ==============================================*/
 	public List<WebElement> checkIfSearchResultsExist(String waitTagName,String tagName) throws TimeoutException {
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		WebElement resultDL = wait.until( ExpectedConditions.presenceOfElementLocated(By.tagName(waitTagName)));
-		List<WebElement> results = resultDL.findElements(By.tagName(tagName));
+		WebDriverWait wait = new WebDriverWait(driver, 10); // Waiter, will timeout after 10 seconds
+		WebElement resultDL = wait.until( ExpectedConditions.presenceOfElementLocated(By.tagName(waitTagName))); // Wait for element waitTagName to appear
+		List<WebElement> results = resultDL.findElements(By.tagName(tagName)); // Get all child elements of the waitTagName element that are tagName
 		return results;
 	}
+	/*=============================================
+	 * (Base case of the IDM's BCC tests)
+	 * Tests the search results when the searchText
+	 * exists within the web app's data and the
+	 * searchScope is quote
+	 ==============================================*/
 	@Test
 	public void searchTextWithResultSearchScopeQuote(){
-		String searchTerm = "software";
-		WebElement searchText = driver.findElement(By.name("searchText"));
-		List<WebElement> searchScopes = null;
-		searchScopes = driver.findElements(By.name("searchScope"));
-		for(int x = 0; x < searchScopes.size();x++){
+		String searchTerm = "software"; // Search term we know exists
+		WebElement searchText = driver.findElement(By.name("searchText")); // Get searchText element
+		List<WebElement> searchScopes = driver.findElements(By.name("searchScope")); // Get the radio buttons
+		for(int x = 0; x < searchScopes.size();x++){ // Loop through the radio buttons and click on the correct searchScope
 			String scopeValue = searchScopes.get(x).getAttribute("value");
 			if(scopeValue.equalsIgnoreCase("quote")){
 				searchScopes.get(x).click();
 				break;
 			}
 		}
-		searchText.sendKeys(searchTerm);
-		searchText.submit();
-		List<WebElement> results = null;
+		searchText.sendKeys(searchTerm); //Enter searchTerm into the textbox
+		searchText.submit();			// Submit to get results
+		List<WebElement> results = null; // Results from search
 		try{
-		results = checkIfSearchResultsExist("dl","dt");
+			results = checkIfSearchResultsExist("dl","dt"); //Get the results from search
 		}
 		catch(TimeoutException e){
-			fail();
+			fail(); // Since we expect results the test fails if we can't find them
 		}
-		if(results.size() == 0)
+		if(results.size() == 0) // Check if we actually have results rather then an empty shell
 			fail();
-		for(int x = 0; x < results.size();x++){
-			//System.out.println(results.get(x).getText());
+		for(int x = 0; x < results.size();x++){ // Loop through results and make sure they are all correct
 			assertTrue(results.get(x).getText().toLowerCase().contains(searchTerm));		
 		}
 	}
+	/*=============================================
+	 * Tests the search results when the searchText
+	 * exists within the web app's data and the
+	 * searchScope is author
+	 ==============================================*/
 	@Test
 	public void searchTextWithResultSearchScopeAuthor() {
-
-		WebElement element = driver.findElement(By.name("searchText"));
-		element.sendKeys("software");
-		WebElement btt = driver.findElement(By.id("author"));
+		WebElement element = driver.findElement(By.name("searchText")); //Get element for searchText
+		element.sendKeys("software"); // Enter in search term
+		WebElement btt = driver.findElement(By.id("author")); // Set searchScope to author
 		btt.click();
-		element.submit();
+		element.submit(); // Submit for results 
 		List<WebElement> results = null;
 		try{
-		results = checkIfSearchResultsExist("dl","dd");
+		results = checkIfSearchResultsExist("dl","dd");//Get the results from search
 		}
 		catch(TimeoutException e){
-			fail();
+			fail(); // Since we expect results the test fails if we can't find them
 		}
-		if(results.size() == 0)
+		if(results.size() == 0) // Check if we actually have results rather then an empty shell
 			fail();
-		for(int x = 0; x < results.size();x++){
+		for(int x = 0; x < results.size();x++){// Loop through results and make sure they are all correct
 			assertTrue(results.get(x).getText().toLowerCase().contains("software"));	
 		}		
 	}
-	
+	/*=============================================
+	 * Tests the search results when the searchText
+	 * exists within the web app's data and the
+	 * searchScope is both
+	 ==============================================*/
 	@Test
 	public void searchTextWithResultSearchScopeBoth() {
-		WebElement element = driver.findElement(By.name("searchText"));
-		element.sendKeys("software");
-		WebElement btt = driver.findElement(By.id("both"));
+		WebElement element = driver.findElement(By.name("searchText")); //Get element for searchText
+		element.sendKeys("software"); // Enter in search term
+		WebElement btt = driver.findElement(By.id("both")); // Set searchScope to both
 		btt.click();
-		element.submit();
-		List<WebElement> resultsText = null;
-		List<WebElement> resultsAuthor = null;
-		resultsText = checkIfSearchResultsExist("dl","dt");
-		resultsAuthor = checkIfSearchResultsExist("dl","dd");
+		element.submit(); // Submit for results 
+		List<WebElement> resultsText = null; //Quote text search results
+		List<WebElement> resultsAuthor = null; //Quote author search results
+		try{
+		resultsText = checkIfSearchResultsExist("dl","dt"); // Get quote text results
+		resultsAuthor = checkIfSearchResultsExist("dl","dd"); // Get quote author results
+		}
+		catch(TimeoutException e){
+			fail(); // Since we expect results the test fails if we can't find them
+		}
 		if(resultsText.size() == 0 || resultsAuthor.size() == 0|| resultsAuthor.size() != resultsText.size())
-			fail();
-		for(int x = 0; x < resultsText.size();x++){
+			fail(); // Makes sure none of the results are empty and they are the same size (since they come in pairs)
+		for(int x = 0; x < resultsText.size();x++){// Loop through results and make sure either the text or author contains the search term
 			assertTrue(resultsText.get(x).getText().toLowerCase().contains("software") || 
 					resultsAuthor.get(x).getText().toLowerCase().contains("software") );	
 		}	
 	}
-	
-		@Test
+	/*=============================================
+	 * Tests the search results when the searchText
+	 * doesn't exist and the searchScope is author
+	 ==============================================*/
+	@Test
 	public void searchTextNoneSearchScopeQuote() {
-
 		WebElement element = driver.findElement(By.name("searchText"));
-		element.clear();
-
+		element.clear(); // Get the searchText and make sure its empty
 		WebElement btt = driver.findElement(By.id("quote"));
-		btt.click();
-		element.submit();
-		WebDriverWait wait = new WebDriverWait(driver, 1);
-
+		btt.click(); // Set searchScope to quote
+		element.submit(); //Submit for results
+		
+		WebDriverWait wait = new WebDriverWait(driver, 1); // Wait for table to appear on the web page
 		WebElement results = wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("table")));
-		try {
+		try {// Check the result area for 
 			WebElement resultArea = results.findElement(By.tagName("tbody")).findElement(By.tagName("tr"))
 					.findElement(By.tagName("td"));
 			resultArea.findElement(By.tagName("p"));
